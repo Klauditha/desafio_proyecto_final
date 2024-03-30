c23faf0511fd49be09d960ccadea47d805f6e4f4
Claudia Villarroel <claudia.villarroel@live.com>
2024-03-30 10:32:22 -0300

    :white_check_mark: Ajustes crear usuario

--- BackEnd/services/user.service.js
+++ BackEnd/services/user.service.js
@@ -34,16 +34,16 @@ class UserService {
         updated_at: new Date().toISOString(),
         deleted: false,
       };
-      
+
       const client = await pool.connect();
       try {
         const maxUserIdQuery = await client.query(
-          "SELECT MAX(user_id) AS max_id FROM users"
+          'SELECT MAX(user_id) AS max_id FROM users'
         );
         const maxUserId = maxUserIdQuery.rows[0].max_id;
 
         const nextUserId = maxUserId ? maxUserId + 1 : 1;
-        
+
         const validatedData = await createUserSchema.validateAsync(data, {
           abortEarly: false,
         });
@@ -51,13 +51,13 @@ class UserService {
         validatedData.user_id = nextUserId;
 
         const query = `
-          INSERT INTO users (user_id, username, password, email, first_name, last_name, phone, region, admin, country, city, district, address, zip_code, created_at, updated_at, deleted)
-          VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)
+          INSERT INTO users (username, password, email, first_name, last_name, phone, region, admin, country, city, district, address, zip_code, created_at, updated_at, deleted)
+          VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)
           RETURNING *
           `;
 
         const result = await client.query(query, [
-          validatedData.user_id,
+          //validatedData.user_id,
           newUser.username,
           newUser.password,
           newUser.email,
