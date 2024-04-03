c38c828d6f166b1ff44011df76e7847052a0afe9
Claudia Villarroel <claudia.villarroel@live.com>
2024-04-03 18:48:01 -0300

    :hammer: seteo rating

--- FrontEnd/src/components/pages/BookDetail.jsx
+++ FrontEnd/src/components/pages/BookDetail.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import { useContext, useEffect, useState } from 'react';
 import { ECommerceContext } from '../../Context/ECommerceContext';
@@ -11,6 +12,7 @@ const BookDetail = () => {
   const [book, setBook] = useState({});
   const [genre, setGenre] = useState({});
   const [author, setAuthor] = useState({});
+  const [rating, setRating] = useState(0);
 
   /**Implementar getbook*/
   const obtenerLibroAPI = () => {
@@ -21,13 +23,34 @@ const BookDetail = () => {
           setBook(response.data.data.book);
           setGenre(response.data.data.genre);
           setAuthor(response.data.data.author);
+          obtenerRatingAPI(response.data.data.book.book_id);
         })
         .catch((error) => {
-          
           console.log(error);
+          alertify.error(
+            'No se ha podido obtener el libro. Por favor, inténtelo de nuevo.')
+        });
+    } catch (error) {
+      alertify.error(
+        'No se ha podido obtener el libro. Por favor, inténtelo de nuevo.')
+      console.log(error);
+    }
+  };
+
+  const obtenerRatingAPI = (book_id) => {
+    try {
+      axios
+        .get(ENDPOINT.rating + '/' + book_id)
+        .then((response) => {
+          setRating(response.data.data.rating);
+        })
+        .catch((error) => {
+          console.log(error);
+      
         });
     } catch (error) {
       console.log(error);
+      
     }
   };
 
@@ -37,7 +60,7 @@ const BookDetail = () => {
   //obtenerLibroAPI();
   return (
     <div>
-      <Productdetail book={book} genre={genre} author={author} />
+      <Productdetail book={book} genre={genre} author={author} rating={rating}/>
     </div>
   );
 };
